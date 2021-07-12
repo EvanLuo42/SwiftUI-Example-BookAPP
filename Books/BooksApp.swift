e366840487e13a73833862abb53dc7d3c7d54f00
EvanLuo42 <ziyun.luo@outlook.com>
2021-07-12 08:47:22 +0800

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Evan Luo on 2021/7/12.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Books/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Books
+//
+//  Created by Evan Luo on 2021/7/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
